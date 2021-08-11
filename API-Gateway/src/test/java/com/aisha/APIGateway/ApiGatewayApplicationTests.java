12d10f3ce396423feeec554be0862b523e3121fe
abcgroupuae <media@abcgroup.ae>
2021-08-11 15:16:46 +0400

    first commit

---
+++ API-Gateway/src/test/java/com/aisha/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisha.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
